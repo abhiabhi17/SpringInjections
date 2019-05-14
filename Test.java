ccfb54d6a375faa400487c51e0db74d340117479
abhiabhi17 <pavadi.abhishek@gmail.com>
2019-05-14 12:47:55 +0530

    new file

---
+++ Test.java
@@ -0,0 +1,17 @@
+package com.main;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.rocket.Rocket;
+
+public class Test {
+public static void main(String[] args) {
+	ApplicationContext ctx= new ClassPathXmlApplicationContext("com/cfg/Beans.xml");
+	Rocket r=ctx.getBean("rocket",Rocket.class);
+	System.out.println(r);
+System.out.println("hi");
+
+	
+}
+}
