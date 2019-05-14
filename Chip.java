c4d146e57bf630941f2b91216f27fe80b4291579
abhiabhi17 <50611304+abhiabhi17@users.noreply.github.com>
2019-05-14 12:36:21 +0530

    Add files via upload

---
+++ Chip.java
@@ -0,0 +1,17 @@
+package com.chip;
+
+public class Chip {
+private Integer chipid;
+private String chipname;
+public void setChipid(Integer chipid) {
+	this.chipid = chipid;
+}
+public void setChipname(String chipname) {
+	this.chipname = chipname;
+}
+@Override
+public String toString() {
+	return "Chip [chipid=" + chipid + ", chipname=" + chipname + "]";
+}
+
+}
