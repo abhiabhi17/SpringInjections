77fe7d4df019cd66ae4bfe9ad257bc9192e2cc34
abhiabhi17 <pavadi.abhishek@Gmail.com>
2019-09-22 18:56:18 +0530

    testcopy added

---
+++ injections/src/main/java/com/main/Test - Copy.java
@@ -0,0 +1,25 @@
+package com.main;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.person.Person;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ApplicationContext ctx=new ClassPathXmlApplicationContext("com/cfg/Beans.xml");
+		Person p1=ctx.getBean("person1",Person.class);
+		Person p2=ctx.getBean("person2",Person.class);
+		System.out.println(p1);
+		System.out.println(p2);
+		System.out.println("changed in git hub");
+		
+
+
+
+
+	}
+
+}
